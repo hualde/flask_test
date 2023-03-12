899e216eeefbf8c8553faceeb0068acd3e70a281
hualde <javhualde@gmail.com>
2023-03-12 23:29:23 +0100

    simplificacion de l aapp

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
 from flask import Flask, url_for, render_template, request, redirect
-from forms import SignupForm
-from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-login_manager = LoginManager(app)
+
 
 posts = [3,4,5]
 
@@ -20,21 +17,3 @@ def index():
 def show_post(slug):
     return render_template("post_view.html", slug_title=slug)
 
-
-@app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>/")
-def post_form(post_id=None):
-    return render_template("admin/post_form.html", post_id=post_id)
-
-@app.route("/signup/", methods=["GET", "POST"])
-def show_signup_form():
-    form = SignupForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        password = form.password.data
-        next = request.args.get('next', None)
-        if next:
-            return redirect(next)
-        return redirect(url_for('index'))                           #si es valido, redirecciona a index
-    return render_template("signup_form.html", form = form)
